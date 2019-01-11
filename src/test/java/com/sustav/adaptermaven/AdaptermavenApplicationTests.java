00607511d2ad39748fc028b7b34df2dd653ae28d
Anton Sustavov <asustavov@unitedthinkers.com>
2019-01-11 08:19:55 +0200

    First commit

---
+++ src/test/java/com/sustav/adaptermaven/AdaptermavenApplicationTests.java
@@ -0,0 +1,17 @@
+package com.sustav.adaptermaven;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdaptermavenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
